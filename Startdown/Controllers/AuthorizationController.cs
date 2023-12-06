552014904f7e9a9f95218376f7345c50715b6a67
Kirill <kirillutrosin8898@gmail.com>
2023-12-06 10:13:09 +0300

    Added Authorization controller + index.html

---
+++ Startdown/Controllers/AuthorizationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Startdown.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        public IActionResult Index() //при переходе по /Autorization
+        {
+            return View();
+        }
+    }
+}
