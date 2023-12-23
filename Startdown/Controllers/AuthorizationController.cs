2db2d0e3c7d13a6516c87bd9265df2d220174592
Kirill <kirillutrosin8898@gmail.com>
2023-12-23 22:19:57 +0300

    Заработала связь с БД

--- Startdown/Controllers/AuthorizationController.cs
+++ Startdown/Controllers/AuthorizationController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace Startdown.Controllers
 {
@@ -7,6 +8,23 @@ namespace Startdown.Controllers
         public IActionResult Index() //при переходе по /Autorization
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult AuthPage(string Login, string Password)
+        {
+            string connstr = "Data Source=DESKTOP-UIR8C5V;Initial Catalog=StartDown_DB;Integrated Security=True";
+            using (SqlConnection conn = new SqlConnection(connstr))
+            {
+                conn.Open();
+
+                string query = $"INSERT INTO [dbo].[User] (Login, Password, ID_Order) VALUES ('{Login}', '{Password}', 1)";
+                SqlCommand command = new SqlCommand(query, conn);
+
+                command.ExecuteNonQuery();
+                conn.Close();
+            }
+            return RedirectToAction();
         }
     }
 }
